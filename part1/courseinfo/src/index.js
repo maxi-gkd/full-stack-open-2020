18c22c82d203d9e39e7b2ae23c9ca9589f93a586
Maxi.Kalaydjian <maxi.kalaydjian@despegar.com>
2020-07-21 18:28:41 -0300

    [PART-1] latest changes

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -21,31 +21,34 @@ const Content = (props) => (
 
 const Total = (props) => (
   <div>
-    <p>Number of exercises {props.parts.reduce(function (a, b) { return a + b.exercises }, 0)}</p>
+    <p>Total number of exercises: {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   </div>
 )
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
